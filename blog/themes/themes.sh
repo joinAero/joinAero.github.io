323f6682c01074021b526052e3d3c39e6cc75e59
joinAero <join.aero@gmail.com>
2016-04-14 21:03:14 +0800

    Enhance themes.sh

--- blog/themes/themes.sh
+++ blog/themes/themes.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
-echo "\033[32mclone \033[35mhexo-theme-next\033[0m"
-git clone https://github.com/iissnan/hexo-theme-next next
+
+theme="hexo-theme-next"
+repo="https://github.com/iissnan/${theme}"
+
+dest="next"
+
+if [[ -a "$dest" ]]; then
+    if [[ -d "$dest" ]]; then
+        echo "\033[32mpull \033[35m${theme}\033[0m"
+        cd $dest; git pull $repo; cd ..
+    else
+        echo "\033[31m${dest} is not a directory\033[0m"
+        exit 1
+    fi
+else
+    echo "\033[32mclone \033[35m${theme}\033[0m"
+    git clone $repo $dest
+fi
+
 exit 0
