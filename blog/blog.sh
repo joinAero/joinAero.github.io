e121cdbd2968e6e32dfc9fa6ccdd157f4af8cf3f
joinAero <join.aero@gmail.com>
2016-04-15 00:02:57 +0800

    Small changes

--- blog/blog.sh
+++ blog/blog.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# echo "\033[32mnpm update\033[0m"
+# npm update
 echo "\033[32minstall \033[35mhexo-deployer-git\033[0m"
 npm install hexo-deployer-git --save
 echo "\033[32minstall themes\033[0m"
