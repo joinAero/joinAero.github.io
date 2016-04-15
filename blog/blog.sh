ccfab8c305e6b98ac1ea419be17abb8e89addd28
joinAero <join.aero@gmail.com>
2016-04-15 16:20:41 +0800

    Add RSS

--- blog/blog.sh
+++ blog/blog.sh
@@ -3,6 +3,8 @@
 # npm update
 echo "\033[32minstall \033[35mhexo-deployer-git\033[0m"
 npm install hexo-deployer-git --save
+echo "\033[32minstall \033[35mhexo-generator-feed\033[0m"
+npm install hexo-generator-feed --save
 echo "\033[32minstall themes\033[0m"
 cd themes/; sh themes.sh; cd ..
 exit 0
